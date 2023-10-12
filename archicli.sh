bf177fa3943a4bbfddabccd32a60203909eca23b
hwo <hwong23@gmail.com>
2023-10-12 17:58:24 -0500

    sh

--- archicli.sh
+++ archicli.sh
@@ -9,6 +9,6 @@
 
 /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/exportSingle-md.ajs -vistaDocumental $4
 
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/markdownDrivingViews-devdocsCLI.ajs -vistaDocumental $4
+# /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/markdownDrivingViews-devdocsCLI.ajs -vistaDocumental $4
 
 /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/exportSingle-pageCLI.ajs -vistaDocumental $4
