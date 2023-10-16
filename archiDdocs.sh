b7d1434298844c201afad1725407dc5b6468eebc
hwo <hwong23@gmail.com>
2023-10-15 21:45:30 -0500

    upd3

--- archiDdocs.sh
+++ archiDdocs.sh
@@ -3,5 +3,5 @@
 # $3: ruta programas ajs
 
 # markdownDrivingViews-devdocsCLI
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/markdownDrivingViews-devdocsCLI.ajs -vistaDocumental $4
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/markdownDrivingViews-devdocsCLI.ajs -vistaDocumental $3
 
