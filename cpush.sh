e6db45d6fab316d0a0f3a756659fd92c97193bb3
hwo <hwong23@gmail.com>
2023-11-03 19:37:16 -0500

    cotizdoc

--- cpush.sh
+++ cpush.sh
@@ -2,5 +2,3 @@ git add .
 git commit -a -m "$1"
 git push
 git pull
-
-sh cpusharq.sh $1
--- cpusharq.sh
+++ cpusharq.sh
@@ -3,10 +3,10 @@
 git checkout arq
 git pull
 
-mv ~/Downloads/tmpr/cmvarchidoc/* .
+cp -R $1/* .
 
 git add .
-git commit -a -m "$1"
+git commit -a -m "$2"
 git push
 
 git checkout main
